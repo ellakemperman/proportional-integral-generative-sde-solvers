217d9c4135d71a639d15ea80d3ba3c95aaa96a73
Ella Kemperman <ella.kemperman@outlook.com>
2026-03-17 16:39:19 +0100

    Implementation of base sde and EM-solver

--- sde.py
+++ sde.py
@@ -8,6 +8,16 @@ class SDE:
     def __init__(self):
         pass
 
-    @abstractmethod
+    def sde(self, x: torch.Tensor, t: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        return self._drift(x, t), self._diffusion(t)
+
     def __call__(self, x: torch.Tensor, t: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        return self.sde(x, t)
+
+    @abstractmethod
+    def _drift(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @abstractmethod
+    def _diffusion(self, t: torch.Tensor) -> torch.Tensor:
         pass
--- solver.py
+++ solver.py
@@ -1,10 +1,12 @@
-from abc import abstractmethod
+"""Definitions of solvers"""
+
+from abc import abstractmethod, ABC
 from sde import SDE
 import torch
 from itertools import pairwise
 
 
-class Solver:
+class Solver(ABC):
 
     def __init__(self, sde: SDE):
         self.__sde = sde
@@ -13,21 +15,17 @@ class Solver:
     def sde(self):
         return self.__sde
 
+    def step_helper(self, x: torch.tensor, t: torch.tensor, dt: torch.tensor, w: torch.Tensor) -> torch.Tensor:
+        drift, diffusion = self.sde(x, t)
+        return x + drift * dt + diffusion * torch.sqrt(torch.abs(dt)) * w
+
     @abstractmethod
     def solve(self, x: torch.tensor) -> torch.tensor:
         pass
 
+    @abstractmethod
     def step(self, x: torch.tensor, t: torch.tensor, dt: torch.tensor) -> torch.tensor:
-        """
-        Computes an SDE step
-
-        :param x:
-        :param t:
-        :param dt:
-        :return:
-        """
-        drift, diffusion = self.sde(x, t)
-
+        pass
 
 
 class EulerMarayumaSolver(Solver):
@@ -38,7 +36,6 @@ class EulerMarayumaSolver(Solver):
         self._discretisation = discretisation
         self._time_steps = map(lambda x: x[1] - x[0], pairwise(discretisation))
 
-
     def solve(self, x: torch.tensor) -> torch.tensor:
         for i, dt in enumerate(self._time_steps):
             t = self._discretisation[i]
@@ -46,4 +43,6 @@ class EulerMarayumaSolver(Solver):
 
         return x
 
-
+    def step(self, x: torch.tensor, t: torch.tensor, dt: torch.tensor) -> torch.tensor:
+        noise = torch.randn_like(x)
+        return self.step_helper(x, t, dt, noise)
