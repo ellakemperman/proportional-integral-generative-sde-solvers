6aeccbe5a509a8f3b540b7aa33f1fce7ef421c23
Ella Kemperman <ella.kemperman@outlook.com>
2026-05-19 17:38:18 +0200

    Implemented conditional probability distribution hex plot

---
+++ pi_solvers/evaluation/data_analysis.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+from KDEpy import FFTKDE
+import matplotlib.pyplot as plt
+from matplotlib.colors import PowerNorm
+
+
+def assign_bin(x: float, bins: np.array):
+    hist = np.histogram(x, bins=bins)[0]
+    if np.max(hist) == 0:
+        return -1
+    return np.argmax(hist)
+
+
+def analyse_pi_data(data_path: str, t_max: float = 80, t_min: float = 0.2, plot_res: int = 200):
+    # Load in t csv
+    ts = pd.read_csv(data_path + "/_t.csv").to_numpy()
+    # Add start time to histogram
+    ts[:, 0] = np.full(ts.shape[0], t_max)
+
+    # Load in h csv
+    hs = pd.read_csv(data_path + "/_h.csv").to_numpy()
+    hs[:, 0] = np.full(hs.shape[0], ts[0, 1] - t_max)
+
+    # Flatten and emove 0 values from ts and hs
+    ts = ts.reshape(-1)
+    hs = hs.reshape(-1)
+    mask = ts != 0
+    ts = ts[mask]
+    hs = hs[mask]
+
+    # Take log of time
+    ts = np.log(ts)
+    hs = np.log(-hs)
+
+    print("Plotting...")
+    # Plotting
+    plt.figure(figsize=(15, 7))
+    plt.title("Time conditional probability density of step size.")
+
+    mesh = plt.hexbin(ts, hs, cmap="inferno", gridsize=(plot_res, plot_res), mincnt=0, bins="log")
+    plt.colorbar(mesh, label='Probability Density')
+
+    # plt.xlim(t_max, t_min)
+    # plt.xscale("log")
+    plt.xlabel("noise/t")
+    plt.ylabel("h")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    analyse_pi_data("../../data/image_testing/pi_2/50NFE_1/data", t_min=0.002, plot_res=100)
